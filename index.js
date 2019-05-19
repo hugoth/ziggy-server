8a566fbe7aa36cfdf10cd81352d81bddaf89af10
Hugo Tessier <htessierhuort@gmail.com>
2019-05-19 17:05:05 +0200

    add Process env MongoDB Uri for Mlab && script for starting app on heroku

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/ziggy-server", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/ziggy-server",
+  { useNewUrlParser: true }
+);
 
 const admin = require("./Routes/admin_routes");
 app.use(admin);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
 });
