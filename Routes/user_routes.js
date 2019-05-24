9528757556dec24f648ef89cffb0c9e3162aecdd
Hugo Tessier <htessierhuort@gmail.com>
2019-05-24 17:22:33 +0200

    add users/:id route for testing the ClientPage on client

--- Routes/user_routes.js
+++ Routes/user_routes.js
@@ -11,4 +11,16 @@ router.get("/users", async (req, res) => {
   }
 });
 
+router.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  //   const user = Users.findById(id);
+  // require Mongoose, not possible whith local DB
+
+  try {
+    res.json(Users[0]);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
