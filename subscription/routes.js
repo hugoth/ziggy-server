80b482c7ec7d6331e7de811752c481f3ec2049f5
belli kieran <bellikieran@mbp-de-denis.lan>
2019-05-30 17:34:41 +0200

    Signup

--- subscription/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("./controllers");
-
-router.post("/subscription", controller.Subscription);
-
-module.exports = router;
