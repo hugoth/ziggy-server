267cd56a3ad93d20e0466a0a909c7eea85e51ff7
Hugo Tessier <htessierhuort@gmail.com>
2019-06-11 10:11:36 +0200

    add update pet route

--- src/api/pets/controllers.js
+++ src/api/pets/controllers.js
@@ -2,7 +2,6 @@ const Pet = require("./model");
 const User = require("../users/model");
 
 async function calculDailyNeeds(req, res) {
-  console.log(req.body.pet);
   const {
     name,
     species,
@@ -165,6 +164,144 @@ async function getPets(req, res) {
   }
 }
 
+async function updatePets(req, res) {
+  try {
+    const searchPet = await Pet.findById(req.body.pet.id);
+    const {
+      name,
+      species,
+      breedfactor,
+      breed,
+      age,
+      weight,
+      sterilized,
+      purebreed,
+      gender,
+      physiology,
+      fitness,
+      healthcare,
+      foodsupply,
+      allergic,
+      appetite,
+      eatcandies,
+      candytype,
+      owner
+    } = req.body.pet;
+
+    let fitnessFactor = 0;
+    let ageFactor = 0;
+    let sterilizedFactor = 0;
+    let physiologyFactor = 0;
+
+    if (species === "chien") {
+      if (age <= 4) {
+        ageFactor = 2;
+      } else if (age > 4 && age <= 9) {
+        ageFactor = 1.6;
+      } else if (age > 9 && age <= 96) {
+        ageFactor = 1.25;
+      } else if (age > 96) {
+        ageFactor = 0.9;
+      }
+      if (fitness == 1) {
+        fitnessFactor = 0.7;
+      } else if (fitness == 2) {
+        fitnessFactor = 0.8;
+      } else if (fitness == 3) {
+        fitnessFactor = 0.9;
+      } else if (fitness == 4) {
+        fitnessFactor = 1;
+      } else if (fitness == 5) {
+        fitnessFactor = 1.1;
+      }
+      if (sterilized === true) {
+        sterilizedFactor = 0.8;
+      } else {
+        sterilizedFactor = 1;
+      }
+      if (physiology == 1) {
+        physiologyFactor = 1.15;
+      } else if (physiology == 2) {
+        physiologyFactor = 1;
+      } else if (physiology == 3) {
+        physiologyFactor = 0.85;
+      }
+    }
+
+    //
+    else if (species === "chat") {
+      if (physiology == 1) {
+        physiologyFactor = 1.15;
+      } else if (physiology == 2) {
+        physiologyFactor = 1;
+      } else if (physiology == 3) {
+        physiologyFactor = 0.85;
+      }
+      if (sterilized === true) {
+        sterilizedFactor = 0.8;
+      } else {
+        sterilizedFactor = 1;
+      }
+    }
+
+    const dogTheoreticalNeeds = weight ** 0.75 * 130;
+    const catTheoreticalNeeds = weight ** 0.67 * 100;
+
+    const dogDailyNeeds =
+      dogTheoreticalNeeds *
+      breedfactor *
+      fitnessFactor *
+      ageFactor *
+      sterilizedFactor *
+      physiologyFactor;
+
+    const catDailyNeeds =
+      catTheoreticalNeeds * sterilizedFactor * physiologyFactor;
+
+    const catFinalNeeds = catDailyNeeds.toFixed(2);
+    const dogFinalNeeds = dogDailyNeeds.toFixed(2);
+
+    let dailyNeeds = 0;
+    if (species === "chien") {
+      dailyNeeds = dogFinalNeeds;
+    } else if (species === "chat") {
+      dailyNeeds = catFinalNeeds;
+    }
+
+    const searchPet = {
+      name,
+      species,
+      breedfactor,
+      breed,
+      age,
+      weight,
+      sterilized,
+      purebreed,
+      gender,
+      physiology,
+      fitness,
+      healthcare,
+      foodsupply,
+      allergic,
+      appetite,
+      eatcandies,
+      candytype,
+      owner,
+      dailyNeeds
+    };
+    await searchPet.save();
+
+    if (species === "chien") {
+      res.json({ dogFinalNeeds, searchPet });
+    } else if (species === "chat") {
+      res.json({ catFinalNeeds, searchPet });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
 module.exports.calculDailyNeeds = calculDailyNeeds;
 module.exports.getSpecies = getSpecies;
 module.exports.getPets = getPets;
+module.exports.updatePets = updatePets;
--- src/api/pets/routes.js
+++ src/api/pets/routes.js
@@ -5,5 +5,6 @@ const controller = require("./controllers");
 router.post("/pet/calculdailyneeds", controller.calculDailyNeeds);
 router.get("/pets:species", controller.getSpecies);
 router.get("/pets", controller.getPets);
+router.post("/pets/update", controller.updatePets);
 
 module.exports = router;
