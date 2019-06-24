3ab76062cf7dd5b14475e2924ca33c2fe28cb16f
Hugo Tessier <htessierhuort@gmail.com>
2019-06-24 14:59:53 +0200

    add routes for subscription after stripe success

--- src/api/orders/routes.js
+++ src/api/orders/routes.js
@@ -6,7 +6,7 @@ const controller = require("./controllers");
 router.get("/orders", controller.getOrders);
 router.get("/order/:id", controller.getOrder);
 router.get("/orders/subscriptions", controller.getSubscriptions);
-router.post("/order/create", controller.createSubscription);
+router.post("/order/create/subscription", controller.createSubscription);
 router.get("/orders/uniqueorders", controller.getUniqueOrders);
 router.get("/orders/:pets", controller.getSpecies);
 router.get("/orders/search/:name", controller.searchOrders);
